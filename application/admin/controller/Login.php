0410b1f174b19d61a27cbccc8243316f09090047
hcd <hcdong2008@126.com>
2017-03-22 08:15:02 +0800

    角色管理完成，用户管理进行中

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,16 @@
+<?php
+	namespace app\admin\controller;
+	use think\Controller;
+	class Login extends Controller{
+		/**
+		 * 登录界面
+		 * @Author    黄传东
+		 * @DateTime  2017-03-15T14:44:06+0800
+		 * @copyright 风险评估中心信息平台
+		 * @return    [type]                   [description]
+		 */
+		public function index(){
+			return 1121212;
+		}
+	}
+?>
---
+++ application/common/controller/Base.php
@@ -0,0 +1,31 @@
+<?php
+	namespace app\common\controller;
+	use think\Controller;
+	use think\Session;
+
+	class Base extends Controller{
+		/**
+		 * 基本类判断用户登录等
+		 * @Author    黄传东
+		 * @DateTime  2017-03-15T14:30:03+0800
+		 * @copyright 风险评估中心信息平台
+		 */
+		public function __construct(){
+			$this->isLogin();
+			parent::__construct();
+		}
+		/**
+		 * 判断是否已登录
+		 * @Author    黄传东
+		 * @DateTime  2017-03-15T14:22:06+0800
+		 * @copyright 风险评估中心信息平台
+		 * @return    boolean                  [description]
+		 */
+		public function isLogin(){
+			$uid=isset($_SESSION['uid'])?intval($_SESSION['uid']):0;
+			if($uid==0){
+				$this->redirect("login/index");
+			}
+		}
+	}
+?>
